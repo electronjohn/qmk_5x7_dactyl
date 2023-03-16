7579058cf02fa94c959969916ac5e44bf77e4d19
electronjohn <johnporterfield@gmail.com>
2023-03-16 17:23:24 -0500

    stable first go

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/electronjohn/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/electronjohn/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,29 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 7
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, F5, F7, C6, F6, D7 } // 4, A2, A0, 5, A1, 6
-
-
-
-
-#define MATRIX_COL_PINS { B6, B5, B2, B4, B3, E6, B1 } // 10, 9, 16, 8, 14, 7, 15
-
-#define MATRIX_ROW_PINS_RIGHT { F6, D4, D7, F7, C6, F5 } // A1, 4, 6, A0, 5, A2
-
-#define MATRIX_COL_PINS_RIGHT { B1, E6, B3, B4, B2, B5, B6} // 15, 7, 14, 8, 16, 9, 10 
-
-#define DIODE_DIRECTION COL2ROW
-
-#define SPLIT_HAND_PIN D1 //2
-
-#define	SPLIT_HAND_PIN_LOW_IS_LEFT
-
-#define SOFT_SERIAL_PIN D0 //3
+//#define EE_HANDS
